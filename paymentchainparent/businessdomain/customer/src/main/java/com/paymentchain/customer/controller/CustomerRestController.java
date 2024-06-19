4e56b655ed01de677b8611d3b78e7c1013fa69b9
GerardoCalistenia <318245630@ciencias.unam.mx>
2024-06-18 21:58:35 -0600

    Transactions Web Client added on CustomerCTRL

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -107,16 +107,30 @@ public class CustomerRestController {
     @GetMapping("/full")
     public Customer getByCode(@RequestParam(name = "code") String code) {
         Customer customer = customerRepository.findByCode(code);
-        List<CustomerProduct> products = customer.getProducts();
-        products.forEach(x -> {
-            String productName = getProductName(x.getProductId());
-            x.setProductName(productName);
-        });
+        if (customer != null) {
+            List<CustomerProduct> products = customer.getProducts();
+
+            //for each product find it name
+            products.forEach(x -> {
+                String productName = getProductName(x.getProductId());
+                x.setProductName(productName);
+            });
+            //find all transactions that belong this account number
+            List<?> transactions = getTransactions(customer.getIban());
+            customer.setTransactions(transactions);
+  
+        }
         return customer;
     }
 
+
+/**
+     * Call Product Microservice , find a product by Id and return it name
+     *
+     * @param id of product to find
+     * @return name of product if it was find
+     */
     private String getProductName(long id) {
-        // Configura un cliente WebClient con un conector HTTP personalizado
         WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
                 .baseUrl("http://localhost:8083/product")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -126,6 +140,32 @@ public class CustomerRestController {
                 .retrieve().bodyToMono(JsonNode.class).block();
         String name = block.get("name").asText();
         return name;
+    }
+
+    /**
+     * Call Transaction Microservice and Find all transaction that belong to the
+     * account give
+     *
+     * @param iban account number of the customer
+     * @return All transaction that belong this account
+     */
+    private List<?> getTransactions(String iban) {
+        WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
+                .baseUrl("http://localhost:8082/transaction")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();       
+        
+        Optional<List<?>> transactionsOptional = Optional.ofNullable(build.method(HttpMethod.GET)
+        .uri(uriBuilder -> uriBuilder
+                .path("/customer/transactions")
+                .queryParam("ibanAccount", iban)
+                .build())
+        .retrieve()
+        .bodyToFlux(Object.class)
+        .collectList()
+        .block());       
+
+        return transactionsOptional.orElse(Collections.emptyList());
     }
 
 }
