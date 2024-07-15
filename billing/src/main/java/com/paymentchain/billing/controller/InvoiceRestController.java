82c756abfbb13674a81149ea9282eb2b47220d05
GerardoCalistenia <318245630@ciencias.unam.mx>
2024-07-15 12:50:07 -0600

    added billing documentation

--- billing/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
+++ billing/src/main/java/com/paymentchain/billing/controller/InvoiceRestController.java
@@ -3,6 +3,11 @@ package com.paymentchain.billing.controller;
 import com.paymentchain.billing.entities.Invoice;
 import com.paymentchain.billing.repository.InvoiceRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
@@ -18,47 +23,51 @@ import org.springframework.web.bind.annotation.PutMapping;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 
+@Tag(name = "Billing APU", description = "API that serve all functionality for management Invoices")
 @RestController
 @RequestMapping("/billing")
 public class InvoiceRestController {
-    
+
     @Autowired
     InvoiceRepository billingRepository;
-    
+
+    @Operation(description = "Return all invoices bundled into Respones", summary = "Return 204 if no data found")
+    @ApiResponses(value ={@ApiResponse(responseCode="200", description = "Success"), 
+    @ApiResponse(responseCode = "500", description = "Internal error")})
     @GetMapping()
     public List<Invoice> list() {
         return billingRepository.findAll();
     }
-    
+
     @GetMapping("/{id}")
-    public ResponseEntity<?>  get(@PathVariable long id) {
-          Optional<Invoice> invoice = billingRepository.findById(id);
+    public ResponseEntity<?> get(@PathVariable long id) {
+        Optional<Invoice> invoice = billingRepository.findById(id);
         if (invoice.isPresent()) {
             return new ResponseEntity<>(invoice.get(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    
+
     @PutMapping("/{id}")
     public ResponseEntity<?> put(@PathVariable String id, @RequestBody Invoice input) {
         return null;
     }
-    
+
     @PostMapping
     public ResponseEntity<?> post(@RequestBody Invoice input) {
         Invoice save = billingRepository.save(input);
         return ResponseEntity.ok(save);
     }
-    
+
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable String id) {
-         Optional<Invoice> dto = billingRepository.findById(Long.valueOf(id));
+        Optional<Invoice> dto = billingRepository.findById(Long.valueOf(id));
         if (!dto.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         billingRepository.delete(dto.get());
         return ResponseEntity.ok().build();
     }
-    
+
 }
--- billing/src/main/java/com/paymentchain/billing/entities/Invoice.java
+++ billing/src/main/java/com/paymentchain/billing/entities/Invoice.java
@@ -1,20 +1,24 @@
 package com.paymentchain.billing.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-
 @Entity
 @Data
+@Schema(name="invoice", description = "Model that represent a invoice in database")
 public class Invoice {
    @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+   @GeneratedValue(strategy = GenerationType.AUTO)
    private long id;
+
+   @Schema(name="customerId", required = true, example = "2", defaultValue = "1", description = "Unique Id of customer thaht represent the owner of invoice.")
    private long customerId;
+   @Schema(name="number", required = true, example = "3", defaultValue = "8", description = "Number given on fiscal invoice")
    private String number;
    private String detail;
-   private double amount;  
+   private double amount;
 }
